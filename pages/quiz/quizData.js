4c7e1c167d70288de1b3eb2aa2f7d1c8bc4079b8
NeaPieroelie <nea.pieroelie@gmail.com>
2024-04-10 13:18:23 -0700

    added a quiz folder, and started on the data

---
+++ pages/quiz/quizData.js
@@ -0,0 +1,7 @@
+export const questions = [
+     {
+        qText: "While crossing the street you should?",
+        type: "radio",
+        choices: ["Run across the street", "Walk across the street", "Skip across the street", "Hop across the street"],
+     }
+]

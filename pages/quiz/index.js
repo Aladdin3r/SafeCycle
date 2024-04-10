ef87be4d34ca43b589d0dd60a61477f3b25b8f28
NeaPieroelie <nea.pieroelie@gmail.com>
2024-04-10 14:09:16 -0700

    adding quiz template, data, and index. I put in 4 questions

--- pages/quiz/index.js
+++ pages/quiz/index.js
@@ -1,5 +1,11 @@
 import styles from '../styles/quiz.module.css'
+import QuizTemplate from '../../components/QuizTemplate/quizTemplate'
+
 
 export default function Quiz() {
-    
+    return (
+        <div>
+            <QuizTemplate
+        </div>
+    )
 }

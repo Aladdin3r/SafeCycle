17f37e1413525ed5ecbf61382640c14fd19c831c
NeaPieroelie <nea.pieroelie@gmail.com>
2024-04-12 23:33:22 -0700

    styled the answer boxes

--- pages/quiz/questionFour.js
+++ pages/quiz/questionFour.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { questions } from "./quizData";
 import Answer from "@/components/Answer";
+import styles from "@/styles/quiz.module.css";
 
 export default function QuestionFour() {
     const currentQuestion = questions[3];
@@ -8,16 +9,17 @@ export default function QuestionFour() {
     return (
         <>
             <h1>{currentQuestion.qText}</h1>
-            {
-                currentQuestion.choices.map((choice, index) => (
-                    <Answer 
-                        key={index} 
-                        name="questionOne"
-                        text={choice} 
-                    />
-                ))
-            }
-
+            <div className={styles.answerContainer}>
+                {
+                    currentQuestion.choices.map((choice, index) => (
+                        <Answer 
+                            key={index} 
+                            name="questionOne"
+                            text={choice} 
+                        />
+                    ))
+                }
+            </div>
             <Link href="./questionThree">
                 <button>Back</button>
             </Link>

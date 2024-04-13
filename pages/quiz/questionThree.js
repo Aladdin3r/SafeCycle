4a5a939c792845f98970359aa4a34f1bc8b1c850
NeaPieroelie <nea.pieroelie@gmail.com>
2024-04-12 23:33:22 -0700

    styled the answer boxes

--- pages/quiz/questionThree.js
+++ pages/quiz/questionThree.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { questions } from "./quizData";
 import Answer from "@/components/Answer";
+import styles from "@/styles/quiz.module.css";
 
 export default function QuestionThree() {
     const currentQuestion = questions[2];
@@ -8,16 +9,17 @@ export default function QuestionThree() {
     return (
         <>
             <h1>{currentQuestion.qText}</h1>
-            {
-                currentQuestion.choices.map((choice, index) => (
-                    <Answer 
-                        key={index} 
-                        name="questionOne"
-                        text={choice} 
-                    />
-                ))
-            }
-
+            <div className={styles.answerContainer}>
+                {
+                    currentQuestion.choices.map((choice, index) => (
+                        <Answer 
+                            key={index} 
+                            name="questionOne"
+                            text={choice} 
+                        />
+                    ))
+                }
+            </div>
             <Link href="./questionTwo">
                 <button>Back</button>
             </Link>
--- pages/quiz/questionTwo.js
+++ pages/quiz/questionTwo.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { questions } from "./quizData";
 import Answer from "@/components/Answer";
+import styles from "@/styles/quiz.module.css";
 
 export default function QuestionTwo() {
     const currentQuestion = questions[1];
@@ -8,16 +9,17 @@ export default function QuestionTwo() {
     return (
         <>
             <h1>{currentQuestion.qText}</h1>
-            {
-                currentQuestion.choices.map((choice, index) => (
-                    <Answer 
-                        key={index} 
-                        name="questionOne"
-                        text={choice} 
-                    />
-                ))
-            }
-
+            <div className={styles.answerContainer}>
+                {
+                    currentQuestion.choices.map((choice, index) => (
+                        <Answer 
+                            key={index} 
+                            name="questionOne"
+                            text={choice} 
+                        />
+                    ))
+                }
+            </div>
             <Link href="./questionOne">
                 <button>Back</button>
             </Link>

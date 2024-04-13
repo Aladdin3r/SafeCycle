a80118f3cf38832cdbf40c75b1d1de0c4fa4dc59
NeaPieroelie <nea.pieroelie@gmail.com>
2024-04-12 23:11:37 -0700

    added files for the quiz and figured out how to make the quiz

--- pages/quiz/questionTwo.js
+++ pages/quiz/questionTwo.js
@@ -1,21 +1,27 @@
 import Link from "next/link";
-import { questions } from "./quiz/quizData";
+import { questions } from "./quizData";
+import Answer from "@/components/Answer";
 
 export default function QuestionTwo() {
-    const currentQuestion = questions[1]; // Get the first question object
+    const currentQuestion = questions[1];
 
     return (
         <>
             <h1>{currentQuestion.qText}</h1>
             {
                 currentQuestion.choices.map((choice, index) => (
-                    <p key={index}>{choice}</p>
+                    <Answer 
+                        key={index} 
+                        name="questionOne"
+                        text={choice} 
+                    />
                 ))
             }
-            <Link href="/questionOne">
+
+            <Link href="./questionOne">
                 <button>Back</button>
             </Link>
-            <Link href="/questionThree">
+            <Link href="./questionThree">
                 <button>Next</button>
             </Link>
         </>

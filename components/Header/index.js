9b0b156f1d159ebc27edc55c12e6ecd0fe11b3c8
NeaPieroelie <nea.pieroelie@gmail.com>
2024-04-18 13:14:18 -0700

    added header file

---
+++ components/Header/index.js
@@ -0,0 +1,19 @@
+import nextConfig from '@/next.config.mjs'
+import styles from './Header.module.css'
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function NavBar() {
+    
+    const title= "Title";
+
+    return( 
+        <>  
+            <div className={`${styles.container}`}>
+                <div className={styles.title}>
+                    <h1>{title}</h1>
+                </div>
+            </div>
+        </>
+    )
+}
--- pages/quiz/quizHome.js
+++ pages/quiz/quizHome.js
@@ -1,10 +1,15 @@
+import Image from "next/image";
+import Header from "@/components/Header";
+
 export default function QuizHome() {
     return(
-        <div className={styles.container}>
-            <div>
-                <h1>Take the Quiz</h1>
+        <>
+            <Header/>
+            <div className={styles.container}>
+                <div>
+                    <h1>Take the Quiz</h1>
+                </div>
             </div>
-        </div>
-        
+        </>
     )
 }

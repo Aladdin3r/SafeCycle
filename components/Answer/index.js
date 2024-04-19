ee2c27b7844299949d2123e387186d34d7847dca
NeaPieroelie <nea.pieroelie@gmail.com>
2024-04-19 15:06:36 -0700

    updating

--- components/Answer/index.js
+++ components/Answer/index.js
@@ -1,19 +1,18 @@
 import { useState } from "react";
 import styles from "./Answer.module.css";
 
-export default function Answer({ text, correctAnswer, onNext, lesson }) {
-    // Define sessionStorage if it's available
+export default function Answer({ option, text, correctAnswer, onNext, lesson }) {
+   
     const sessionStorage = typeof window !== 'undefined' ? window.sessionStorage : null;
 
-    // Initialize selected state with the value stored in session storage or false if not available
     const [selected, setSelected] = useState(sessionStorage?.getItem('selectedAnswer') === text);
     const [lessonShown, setLessonShown] = useState(false);
 
     const handleAnswerClick = () => {
         setSelected(true);
-        sessionStorage?.setItem('selectedAnswer', text); // Store selected answer in session storage if available
+        sessionStorage?.setItem('selectedAnswer', text);
         if (text !== correctAnswer) {
-            setLessonShown(true); // Show lesson if the answer is wrong
+            setLessonShown(true);
         }
         onNext(text === correctAnswer);
     };
@@ -22,7 +21,7 @@ export default function Answer({ text, correctAnswer, onNext, lesson }) {
         <div>
             <div className={`${styles.answerBox} ${selected ? styles.selected : ''} ${selected && text === correctAnswer ? styles.correct : ''}`} onClick={handleAnswerClick}>
                 <input type="radio" name="answer" className={styles.radioButton} checked={selected} onChange={() => {}} />
-                <label className={styles.label}>{text}</label>
+                <label className={styles.label}>{option}. {text}</label>
             </div>
             {lessonShown && (
                 <div className={styles.lesson}>

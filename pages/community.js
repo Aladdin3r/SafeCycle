dad42ffea3ee4afe27eba7238ac859f83062c4da
NeaPieroelie <nea.pieroelie@gmail.com>
2024-04-25 13:29:13 -0700

    created hazards and communty page

--- pages/community.js
+++ pages/community.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Header from "@/components/Header";
 import NavBar from "@/components/NavBar";
 import styles from "@/styles/Community.module.css";
+import Image from "next/image";
 
 export default function Community() {
     return(
@@ -15,6 +16,25 @@ export default function Community() {
             <main className={`${styles.main}`}>
                 <div className={styles.navcontainer}>
                     <Header title="Community"/>
+                    <div className={styles.cards}>
+                        <div className={styles.nearbyActivity}>
+                            <div className={styles.cardContainer}>
+                                <div className={styles.nearbyImage}>
+                                    <Image src="/icons/nearby-activity-3D.svg" width={141.967} height={103}/>
+                                </div>
+                            </div>
+                            <p>Nearby Activity</p>
+                        </div>
+                        <div className={styles.nearbyActivity}>
+                            <div className={styles.cardContainer}>
+                                <div className={styles.nearbyImage}>
+                                    <Image src="/images/faq.webp" width={120} height={130}/>
+                                </div>
+                            </div>
+                            <p>FAQ</p>
+                        </div>
+                    </div>
+
                     <NavBar/>
                 </div>
             </main>

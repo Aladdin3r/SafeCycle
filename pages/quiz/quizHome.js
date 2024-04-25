d08ffd8873eb1f4bb1f846f308e0cbad3609e75e
Andrew <andrewturitsa@gmail.com>
2024-04-25 11:34:37 -0700

    added pie chart with placeholder values

--- pages/quiz/quizHome.js
+++ pages/quiz/quizHome.js
@@ -5,6 +5,7 @@ import styles from "@/styles/quiz.module.css";
 import Link from "next/link";
 import NavBar from "@/components/NavBar";
 import Button from "@/components/Button"
+import BarChart from "@/components/PieChart";
 
 export default function QuizHome() {
     return(
@@ -20,6 +21,12 @@ export default function QuizHome() {
                 <div>
                     <div className={styles.quizHomeTitle}>
                         <h3>Ready to see if you’re a good pedestrian? <br/> Take the Quiz!</h3>
+                    </div>
+                    <div className={styles.quizHomeDescription}>
+                        <h3>To this day road related injuries are a leading cause of hospitalizations in Canada. <br/> Take this quiz to remind yourself of the safest practices for pedestrians and cyclists to follow!</h3>
+                    </div>
+                    <div className={styles.quizHomeGraph}>
+                        <BarChart />
                     </div>
                     <div className={styles.startButtonContainer}>
                             <Button 

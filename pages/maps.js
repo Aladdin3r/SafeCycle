63f51cd7313b11e62222fdb0dd438facee014eeb
Rawan Aladdin <raladdin@my.bcit.ca>
2024-05-02 02:24:23 -0700

    added pull up page component, has closing errors to be fixed

--- pages/maps.js
+++ pages/maps.js
@@ -1,29 +1,34 @@
+import React, { useState } from "react";
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
 import NavBar from "@/components/NavBar";
-import SearchBar from "@/components/SearchBar";
 import SearchBarButton from "@/components/SearchBarButton";
 import MapPage from "@/components/Map";
 import PullUpPage from "@/components/PullUpPage";
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>SafeCycle</title>
-        <meta name="description" content="Home Page" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon3.png" />
-      </Head>
-      <main className={`${styles.main}`}>
-        <div className={styles.container}>
-          {/* <SearchBar /> */}
-          <SearchBarButton/>
-          <MapPage />
-          <NavBar />
-          <PullUpPage/>
-        </div>
-      </main>
-    </>
-  );
+    const [pullPageVisible, setPullPageVisible] = useState(false);
+
+    const togglePullPage = () => {
+        setPullPageVisible(!pullPageVisible);
+    };
+
+    return (
+        <>
+            <Head>
+                <title>SafeCycle</title>
+                <meta name="description" content="Home Page" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon3.png" />
+            </Head>
+            <main className={styles.main}>
+                <div className={styles.container}>
+                    <SearchBarButton onClick={togglePullPage} />
+                    <PullUpPage isVisible={pullPageVisible} toggleVisibility={togglePullPage} />
+                    <MapPage />
+                    <NavBar />
+                </div>
+            </main>
+        </>
+    );
 }

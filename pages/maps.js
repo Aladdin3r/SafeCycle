05147188492ff273488a4e5a4c549bf248d5c33c
Rawan Aladdin <raladdin@my.bcit.ca>
2024-05-01 23:48:32 -0700

    changed meta description

--- pages/maps.js
+++ pages/maps.js
@@ -3,7 +3,7 @@ import styles from "@/styles/Home.module.css";
 import NavBar from "@/components/NavBar";
 import SearchBar from "@/components/SearchBar";
 import MapPage from "@/components/Map";
-
+import PullUpPage from "@/components/PullUpPage";
 
 export default function Home() {
   return (
@@ -19,7 +19,7 @@ export default function Home() {
           <SearchBar />
           <MapPage />
           <NavBar />
-          
+          <PullUpPage/>
         </div>
       </main>
     </>

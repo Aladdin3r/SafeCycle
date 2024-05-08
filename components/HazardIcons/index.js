ef92784ae9f75b40b94949971eeba4b65aa696cb
NeaPieroelie <nea.pieroelie@gmail.com>
2024-05-07 22:49:44 -0700

    updating

--- components/HazardIcons/index.js
+++ components/HazardIcons/index.js
@@ -1,13 +1,16 @@
 import Image from "next/image";
 import styles from "./HazardIcons.module.css";
+import Link from "next/link";
 
-const HazardIcons = ({ imageUrl, altText, width, height, title }) => {
+const HazardIcons = ({ imageUrl, altText, width, height, title, page }) => {
   return (
     <div className={styles.iconContainer}>
-      <button className={styles.iconButton}>
-        <Image src={imageUrl} alt={altText} width={width} height={height} />
-      </button>
-      <p className={styles.iconTitle}>{title}</p>
+      <Link href={page} >
+        <button className={styles.iconButton}>
+          <Image src={imageUrl} alt={altText} width={width} height={height} />
+        </button>
+        <p className={styles.iconTitle}>{title}</p>
+      </Link>
     </div>
   );
 };

19222979025633e9ccb76ac52c6d3702c9c73433
Andrew <andrewturitsa@gmail.com>
2024-05-08 00:19:27 -0700

    added results page

--- pages/quiz/endPage.js
+++ pages/quiz/endPage.js
@@ -2,10 +2,13 @@ import Link from "next/link"
 import styles from "@/styles/quiz.module.css"
 
 export default function EndPage() {
+    const correctCount = parseInt(window.sessionStorage.getItem('correctCount')) || 0;
+
     return(
         <>
         <div className={styles.end}>
             <p>Thanks for taking the quiz!</p>
+            <p>Number of questions answered correctly: {correctCount} out of 4</p>
             <Link href="/">
                 <button className={styles.button}>Return to Home</button>
             </Link>

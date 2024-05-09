ba35892353f932cab72315fdc51be5eb6160037c
Rawan Aladdin <raladdin@my.bcit.ca>
2024-05-09 12:00:37 -0700

    navbar active states

--- pages/profile.js
+++ pages/profile.js
@@ -9,18 +9,13 @@ export default function Profile() {
   return(
     <>
             <Head>
-                <title>SafeCycle – Profile</title>
+                <title>SafeCycle - Profile</title>
                 <meta name="description" content="Community Page" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon3.png" /> 
             </Head>
             <main className={`${styles.main}`}>
-              <div>
-              <Image className={`${styles.profileIcon}`} src="/icons/profile-icon.svg" width={57} height={55}/>
-              <h2 className={`${styles.profileName}`}>John Jo</h2>
-              </div>
-
-
+              <Header title={"Profile"}/>
               <h2 className={`${styles.catagory}`}>
                 <Image className={`${styles.catagoryIcon}`} src="/icons/profile-page-locations.svg" width={25} height={25}/>
                 <div className={`${styles.catagoryText}`}>Locations</div>
@@ -34,7 +29,7 @@ export default function Profile() {
                 <Image className={`${styles.catagoryIcon}`} src="/icons/profile-page-settings.svg" width={24} height={24}/>
                 <div className={`${styles.catagoryText}`}>Settings</div>
               </h2>
-                <div className={`${styles.selectorRectangle}`}> <div className={`${styles.selectorRectangleText}`}>App Apearance</div></div>
+                <div className={`${styles.selectorRectangle}`}> <div className={`${styles.selectorRectangleText}`}>App Appearance</div></div>
                 <div className={`${styles.selectorRectangle}`}> <div className={`${styles.selectorRectangleText}`}>Language</div></div>
                 <div className={`${styles.selectorRectangle}`}> <div className={`${styles.selectorRectangleText}`}>FAQ</div></div>
                            
@@ -46,10 +41,10 @@ export default function Profile() {
               </h2>
                 <div className={`${styles.selectorRectangle}`}> <div className={`${styles.selectorRectangleText}`}>Alert Frequency</div></div>
                 <div className={`${styles.selectorRectangle}`}> <div className={`${styles.selectorRectangleText}`}>Alert Places</div></div>
-                <div className={`${styles.selectorRectangle}`}> <div className={`${styles.selectorRectangleText}`}>Abvanced Aler Settings</div></div>
+                <div className={`${styles.selectorRectangle}`}> <div className={`${styles.selectorRectangleText}`}>Advanced Alert Settings</div></div>
               
 
-
+  
                 <div className={styles.container}>
 
                 </div>

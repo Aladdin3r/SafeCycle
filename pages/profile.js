3e9161e8814ce97d7cfba242533c294032c11ecc
Andrew <andrewturitsa@gmail.com>
2024-05-13 08:36:25 -0700

    kind of fixed the profile's icons

--- pages/profile.js
+++ pages/profile.js
@@ -15,59 +15,40 @@ export default function Profile() {
                 <link rel="icon" href="/favicon3.png" /> 
             </Head>
             <main className={`${styles.main}`}>
+              <Header title={"Profile"}/>
               <div>
               <Image className={`${styles.profileIcon}`} src="/icons/profile-icon.svg" width={57} height={55}/>
               <h2 className={`${styles.profileName}`}>John Jo</h2>
               </div>
 
-               
-              {/* <h2 className={`${styles.catagory}`}>
+              <div> 
+              <h2 className={`${styles.catagory}`}>
                 <Image className={`${styles.catagoryIcon}`} src="/icons/profile-page-locations.svg" width={25} height={25}/>
                 <div className={`${styles.catagoryText}`}>Locations</div>
-              <Header title={"Profile"}/>
-              </h2> */}
-              
-              
-              <h2 className={`${styles.category}`}>
-                <Image className={`${styles.categoryIcon}`} src="/icons/profile-page-locations.svg" width={25} height={25}/>
-                <div className={`${styles.categoryText}`}>Locations</div>
               </h2>
-              
-              
+              </div>
                 <div className={`${styles.selectorRectangle}`}> <div className={`${styles.selectorRectangleText}`}>Saved</div></div>
                 <div className={`${styles.selectorRectangle}`}> <div className={`${styles.selectorRectangleText}`}>Home</div></div>
 
-              <br/>
               
-              <h2 className={`${styles.catagory}`}>
-                <Image className={`${styles.catagoryIcon}`} src="/icons/profile-page-settings.svg" width={24} height={24}/>
-                <div className={`${styles.catagoryText}`}>Settings</div>
-              </h2>
-              
-                <div className={`${styles.selectorRectangle}`}> <div className={`${styles.selectorRectangleText}`}>App Apearance</div></div>
               <div>
               <h2 className={`${styles.category}`}>
                 <Image className={`${styles.categoryIcon}`} src="/icons/profile-page-settings.svg" width={24} height={24}/>
                 <div className={`${styles.categoryText}`}>Settings</div>
               </h2>
-              
+              </div>
                 <div className={`${styles.selectorRectangle}`}> <div className={`${styles.selectorRectangleText}`}>App Appearance</div></div>
                 <div className={`${styles.selectorRectangle}`}> <div className={`${styles.selectorRectangleText}`}>Language</div></div>
                 <div className={`${styles.selectorRectangle}`}> <div className={`${styles.selectorRectangleText}`}>FAQ</div></div>
                            
                 
-              
-              <h2 className={`${styles.catagory}`}>
-                <Image className={`${styles.catagoryIcon}`} src="/icons/profile-page-notifications.svg" width={24} height={24}/>
-                <div className={`${styles.catagoryText}`}>Notifications</div>
-                </h2>
-              </div>
-              
+                
+              <div>
               <h2 className={`${styles.category}`}>
                 <Image className={`${styles.categoryIcon}`} src="/icons/profile-page-notifications.svg" width={24} height={24}/>
                 <div className={`${styles.categoryText}`}>Notifications</div>
               </h2>
-              
+              </div>
                 <div className={`${styles.selectorRectangle}`}> <div className={`${styles.selectorRectangleText}`}>Alert Frequency</div></div>
                 <div className={`${styles.selectorRectangle}`}> <div className={`${styles.selectorRectangleText}`}>Alert Places</div></div>
                 <div className={`${styles.selectorRectangle}`}> <div className={`${styles.selectorRectangleText}`}>Advanced Alert Settings</div></div>

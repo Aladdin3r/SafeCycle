cfc8fa4f604910427b0d5fb9589c55fe98a2d4c0
Rawan Aladdin <raladdin@my.bcit.ca>
2024-05-15 00:06:13 -0700

    head added

--- pages/search.js
+++ pages/search.js
@@ -1,13 +1,14 @@
 import styles from "@/styles/Home.module.css"
 import SearchBarButton from "@/components/SearchBar";
 import TextButton from "@/components/SearchBar"
+import Head from "next/head";
 
 export default function Search() {
     return(
         <>
         <Head>
             <title>SafeCycle - Search Page</title>
-            <meta name="description" content="SafeCycle is your ultimate pedestrian safety and navigation app. 
+            <meta name="description" content="Search Page. SafeCycle is your ultimate pedestrian safety and navigation app. 
             Find the best and most recent routes and navigate your day without hazards." />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link rel="icon" href="/favicon3.png" />

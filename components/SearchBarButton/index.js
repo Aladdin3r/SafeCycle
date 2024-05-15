2a9c4652a282682bd7bf6299b72a0e9a1495d843
NeaPieroelie <nea.pieroelie@gmail.com>
2024-05-14 23:18:18 -0700

    fixed search button

--- components/SearchBarButton/index.js
+++ components/SearchBarButton/index.js
@@ -7,12 +7,12 @@ export default function SearchBarButton() {
 
     return (
         <div className={styles.container}>
-                <a className={styles.searchbutton}>
+                <div className={styles.searchbutton}>
                     <div className={styles.search}>
                         <Image className={styles.searchIcon} src="/icons/search.svg" width={20} height={20} />
                         Search place
                     </div>
-                </a>
+                </div>
         </div>
     );
 };

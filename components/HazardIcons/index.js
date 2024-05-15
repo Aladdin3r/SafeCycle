84d3a2b24e14956199fc409838d5591a0759f49a
Andrew <andrewturitsa@gmail.com>
2024-05-14 21:28:01 -0700

    added tabindex to community and hazards page

--- components/HazardIcons/index.js
+++ components/HazardIcons/index.js
@@ -2,11 +2,14 @@ import Image from "next/image";
 import Link from "next/link";
 import styles from "./HazardIcons.module.css";
 
+let tabIndexCounter = 2;
+
+const tabIndex = tabIndexCounter++;
 const HazardIcons = ({ imageUrl, altText, width, height, title, onClick }) => {
   return (
-    <div className={styles.iconContainer}>
-      <Link href="/addReport">
-          <button className={styles.iconButton}>
+    <div className={styles.iconContainer} >
+      <Link tabIndex={tabIndex} href="/addReport">
+          <button tabIndex={-1} className={styles.iconButton}>
             <Image src={imageUrl} alt={altText} width={width} height={height} />
           </button>
       </Link>
